80e5eb5e91d71f4c402aad8a7ad72f1ac3dadcda
DanielVec <vechioladaniel@gmail.com>
2022-02-12 16:40:28 -0800

    create functions for adding, subtracting, multiplying, dividing and operating

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+function operate(operator, a, b) {
+    if (operator = '+') add(a, b);
+    if (operator = '-') subtract(a, b);
+    if (operator = '*') multiply(a, b);
+    if (operator = '/') divide(a, b);
+}
