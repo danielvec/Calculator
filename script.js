d87704c355e8f982851ba47aaf7accdb4282b5f3
DanielVec <vechioladaniel@gmail.com>
2022-02-13 23:08:39 -0800

    use javascript to populate display with values and operators. Calculator now functions with adding and subtracting

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a, b) {
@@ -11,12 +11,59 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    console.log(a / b);
+    return a / b;
 }
 
 function operate(operator, a, b) {
-    if (operator = '+') add(a, b);
-    if (operator = '-') subtract(a, b);
-    if (operator = '*') multiply(a, b);
-    if (operator = '/') divide(a, b);
+    if (operator === '+') return add(a, b);
+    if (operator === '-') return subtract(a, b);
+    if (operator === '*') return multiply(a, b);
+    if (operator === '/') divide(a, b);
 }
+let display = document.querySelector("#display");
+let cont = display.textContent;
+function toDisplay() {
+    document.querySelectorAll(".number").forEach(number => {
+        number.addEventListener('click', () => {
+             display.textContent += number.textContent;
+             console.log("step 1", display.textContent);
+          })
+
+    })
+
+    document.querySelectorAll(".operator").forEach(operator => {
+        operator.addEventListener('click', () => {
+            x = display.textContent;
+            display.textContent += operator.textContent;
+            y = operator.textContent;
+            console.log("step 2", x, y);
+            //calculate();
+        })
+     
+    })
+
+    let equals = document.querySelector(".equals")
+        equals.addEventListener('click', () => {
+            zArray = display.textContent.split(/[+-]/g);
+            z = zArray[zArray.length - 1];
+            console.log(zArray, z)
+             display.textContent = operate(y,x,z);
+          })
+
+    
+    //toDisplay();
+}
+
+function calculate() {
+    document.querySelectorAll(".number").forEach(number => {
+        number.addEventListener('click', () => {
+            display.textContent += number.textContent;
+            let z = display.textContent;
+            console.log("step 3", z);
+            })
+    })
+
+}
+
+toDisplay();
+
