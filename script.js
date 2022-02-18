6a17a0f75529d5c422dd02dd99c5b1b8dc312591
DanielVec <vechioladaniel@gmail.com>
2022-02-17 21:41:45 -0800

    add exponent function, add delete button, and rearrange calculator buttons

--- script.js
+++ script.js
@@ -14,11 +14,16 @@ function divide(a, b) {
     return a / b;
 }
 
+function exponent(a, b) {
+    return a ** b;
+}
+
 function operate(operator, a, b) {
     if (operator === '+') return add(a, b);
     if (operator === '-') return subtract(a, b);
     if (operator === 'x') return multiply(a, b);
     if (operator === '/') return divide(a, b);
+    if (operator === '^') return exponent(a, b);
 }
 let display = document.querySelector("#display");
 let calculator = document.querySelector("#calculator")
@@ -42,7 +47,7 @@ function toDisplay() {
             else {display.textContent += decimal.textContent;}
           })
     })
-
+    
     document.querySelectorAll(".operator").forEach(operator => {
         operator.addEventListener('click', () => {
             if (display.textContent.length > 7) {
@@ -54,7 +59,7 @@ function toDisplay() {
 
     let equals = document.querySelector(".equals")
         equals.addEventListener('click', () => {
-            numArray = display.textContent.split(/[\/x+-]/g);
+            numArray = display.textContent.split(/[\/x^+-]/g);
             opArray = display.textContent.split(/[.\d]/g).filter(Boolean);
 
             if (numArray.length < 3) {
@@ -71,10 +76,19 @@ function toDisplay() {
           })
         }
 
-        let clear = document.querySelector("#clear")
+        
+    let clear = document.querySelector("#clear")
         clear.addEventListener('click', () => {
          display.textContent = "";
           })
+
+    let deletion = document.querySelector("#delete")
+        deletion.addEventListener('click', () => {
+        displayArray = Array.from(display.textContent);
+        displayArray.pop()
+        let newDisplay = displayArray.join('')
+        display.textContent = newDisplay;
+           })
         
 
 toDisplay();
